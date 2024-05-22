d07707ff9a8efef297c6af3d24c1762f48c775c3
= <=>
2024-05-22 09:40:29 +0200

    Add find, exists, size, empty, keys, and values methods to HashTable interface

--- HaschTable.h
+++ HaschTable.h
@@ -3,7 +3,12 @@ template <typename K, typename V>
 class HashTable{
     virtual void insert(K key, V value) = 0;
     virtual void remove(K key) = 0;
-    virtual int findKey(K key) = 0;
+    virtual V find(K key) = 0;
+    virtual bool exists(K key) = 0;
+    virtual void size() = 0;
+    virtual void empty() = 0;
+    virtual void keys() = 0;
+    virtual void values() = 0;
     virtual void print() = 0;
     virtual ~HashTable() = default;
 };
